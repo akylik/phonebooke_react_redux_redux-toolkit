75e8b9c08b9f53394021f404afd1b36bf6579c41
Kulyk Oleksandr <70694467+akylik@users.noreply.github.com>
2021-06-30 19:03:18 +0300

    Updates

---
+++ precache-manifest.94fa3a387b84b639abeaafe9f1193e79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f4d2e64a0af4c8897cd0b37056e4d3",
+    "url": "/phonebooke_react_redux_redux-toolkit/index.html"
+  },
+  {
+    "revision": "24f0cd4b8dc40ac951e9",
+    "url": "/phonebooke_react_redux_redux-toolkit/static/css/2.1a215020.chunk.css"
+  },
+  {
+    "revision": "4af5f343c86899c6e9f1",
+    "url": "/phonebooke_react_redux_redux-toolkit/static/css/main.f9fa7f78.chunk.css"
+  },
+  {
+    "revision": "24f0cd4b8dc40ac951e9",
+    "url": "/phonebooke_react_redux_redux-toolkit/static/js/2.cd68899d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phonebooke_react_redux_redux-toolkit/static/js/2.cd68899d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af5f343c86899c6e9f1",
+    "url": "/phonebooke_react_redux_redux-toolkit/static/js/main.bc1f3161.chunk.js"
+  },
+  {
+    "revision": "dcac2a925c85ba11dd5e",
+    "url": "/phonebooke_react_redux_redux-toolkit/static/js/runtime-main.2451a3f5.js"
+  }
+]);
